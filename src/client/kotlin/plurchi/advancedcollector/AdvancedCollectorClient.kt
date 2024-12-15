c1196bcf17fca92e43ae6657aa662ae10a1f49d3
pLurchi <j.novak@jjnet.eu>
2024-12-15 15:00:21 +0100

    Fabric template plugin

---
+++ src/client/kotlin/plurchi/advancedcollector/AdvancedCollectorClient.kt
@@ -0,0 +1,9 @@
+package plurchi.advancedcollector
+
+import net.fabricmc.api.ClientModInitializer
+
+object AdvancedCollectorClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/plurchi/advancedcollector/AdvancedCollectorDataGenerator.kt
@@ -0,0 +1,9 @@
+package plurchi.advancedcollector
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object AdvancedCollectorDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
